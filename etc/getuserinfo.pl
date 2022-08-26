7d39a1370f0758240c4b2bcc761317fe21da2894
hgot07 <hgot07@gmail.com>
2022-08-26 15:39:09 +0900

    Added per-user ExpirationDate support.

---
+++ etc/getuserinfo.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+sub getuserinfo {
+	my $webuser = $_[0];
+
+# Write your own code here to set per-user parameters.
+	#$userID = 'name@example.com';
+	#$passwd = 'somePassword';
+	#$ExpirationDate = '2023-01-05T00:00:00Z';
+
+	return(0);
+}
